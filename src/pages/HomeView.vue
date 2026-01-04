bc045ee2fd41e46213e994b396b1348f69cb14a7
AIdysh Baldan <aidysh0324@gmail.com>
2026-01-04 23:01:44 +0700

    feat: добавлен блок контейнер, чтобы зафиксировать header

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
-  <ProductList />
+  <div class="container">
+    <ProductList />
+  </div>
 </template>
 
 <script setup>
 import ProductList from "../components/ProductList.vue";
 </script>
+
+<style scoped>
+.container {
+  padding-top: 7vh;
+  background-color: white;
+}
+</style>
